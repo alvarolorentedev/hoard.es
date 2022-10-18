09509615d86df2f815a7b61818c55d3defe7fb30
kanekotic <me@kanekotic.com>
2022-10-18 16:39:23 +0200

    Alvaro | added back docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -64,6 +64,12 @@ const config = {
           src: 'img/logo.svg',
         },
         items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Tutorials',
+          },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/kanekotic/hoard.es',
