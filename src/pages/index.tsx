5be41ea78872d00dc265a6c27a88374e909f5204
kanekotic <me@kanekotic.com>
2022-10-20 09:54:47 +0200

    Alvaro | improved homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,12 +5,15 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import Svg from '@site/static/img/logo.svg'
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
+      
+        <Svg role="img" />
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
       </div>
